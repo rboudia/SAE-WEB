3d7342b766de54370251458cf62efab1bd8a7963
aboudjemai <aboudjemai@iut.univ-paris8.fr>
2023-12-11 10:53:11 +0100

    test

--- template.php
+++ template.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Site de tout le monde</title>
+    <title>Site de tout le Monde </title>
     <style>
         * {
     margin: 0;
