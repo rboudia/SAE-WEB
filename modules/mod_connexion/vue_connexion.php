8d3c220f4c1d6ed618414c2cd3e0fb3732a8090c
aboudjemai <aboudjemai@iut.univ-paris8.fr>
2023-12-14 14:38:23 +0100

    css

--- modules/mod_connexion/vue_connexion.php
+++ modules/mod_connexion/vue_connexion.php
@@ -1,22 +1,34 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Titre de votre page</title>
+    <link rel="stylesheet" type="text/css" href="Css.css">
+</head>
+<body>
+
 <?php
 require_once 'vue_generique.php';
 
 class VueConnexion extends VueGenerique {
-  public function form_inscription() {
+    public function form_inscription() {
         ?>
-        <h2>Inscription</h2>
-        <?php if(isset($_SESSION['erreur'])){
+        <div>
+            <h2>Inscription</h2>
+            <?php if(isset($_SESSION['erreur'])){
+                ?>
+                <div style="color:red"><?=$_SESSION['erreur']?></div><br>
+                <?php
+                unset($_SESSION['erreur']);
+            }
             ?>
-            <div style="color:red"><?=$_SESSION['erreur']?></div><br>
-            <?php
-            unset($_SESSION['erreur']);
-        }
-        ?>
-        <form method="post" action="index.php?module=connexion&action=inscription">
-        Nom d'utilisateur : <input type="text" name="login" required><br>
-        Mot de passe : <input type="password" name="mdp" required><br>
-        <input type="submit" value="S'inscrire">
-        </form>
+            <form method="post" action="index.php?module=connexion&action=inscription">
+                Nom d'utilisateur : <input type="text" name="login" required><br>
+                Mot de passe : <input type="password" name="mdp" required><br>
+                <input type="submit" value="S'inscrire">
+            </form>
+        </div>
         <?php
     }
 
@@ -24,30 +36,33 @@ class VueConnexion extends VueGenerique {
         if (isset($_SESSION['user'])) {
             $utilisateur = $_SESSION['user'];
             ?>
-            <li>Vous êtes déjà connecté sous l'identifiant <?= $utilisateur['login'] ?> !</li>
-            <li><a href="index.php?module=connexion&action=deconnexion">Déconnexion</a></li>
-             <?php
-            } else {
-        ?>
-        <h2>Connexion</h2>
-        <?php if(isset($_SESSION['erreur'])){
-            ?>
-            <div style="color:red"><?=$_SESSION['erreur']?></div><br>
+            <div>
+                <li>Vous êtes déjà connecté sous l'identifiant <?= $utilisateur['login'] ?> !</li>
+                <li><a href="index.php?module=connexion&action=deconnexion">Déconnexion</a></li>
+            </div>
             <?php
-            unset($_SESSION['erreur']);
-        }
-        ?>
-        <form method="post" action="index.php?module=connexion&action=connexion">
-        Nom d\'utilisateur : <input type="text" name="login" required><br>
-        Mot de passe : <input type="password" name="mdp" required><br>
-        <input type="submit" value="Se connecter">
-        </form>
-        <?php
+        } else {
+            ?>
+            <div>
+
+                <h2 class="connexion">Connexion</h2>
+                <?php if(isset($_SESSION['erreur'])){
+                    ?>
+                    <div style="color:red"><?=$_SESSION['erreur']?></div><br>
+                    <?php
+                    unset($_SESSION['erreur']);
+                }
+                ?>
+                <form method="post" action="index.php?module=connexion&action=connexion">
+                    Nom d'utilisateur : <input type="text" name="login" required><br>
+                    Mot de passe : <input type="password" name="mdp" required><br>
+                    <input type="submit" value="Se connecter">
+                </form>
+            </div>
+            <?php
         }
     }
 }
-
 ?>
-
-
- 
+</body>
+</html>
