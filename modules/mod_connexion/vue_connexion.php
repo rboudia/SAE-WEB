9fc455d1c73467fdf8e4304247678135e86a751e
aboudjemai <aboudjemai@iut.univ-paris8.fr>
2023-12-14 15:18:04 +0100

    modifs css

--- modules/mod_connexion/vue_connexion.php
+++ modules/mod_connexion/vue_connexion.php
@@ -1,13 +1,3 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Titre de votre page</title>
-    <link rel="stylesheet" type="text/css" href="Css.css">
-</head>
-<body>
-
 <?php
 require_once 'vue_generique.php';
 
@@ -15,17 +5,18 @@ class VueConnexion extends VueGenerique {
     public function form_inscription() {
         ?>
         <div>
-            <h2>Inscription</h2>
+            <link rel="stylesheet" type="text/css" href="modules/mod_connexion/Css.css">
+            <h2 class="inscription">Inscription</h2>
             <?php if(isset($_SESSION['erreur'])){
                 ?>
-                <div style="color:red"><?=$_SESSION['erreur']?></div><br>
+                <div style="color:red; text-align: center;"><?=$_SESSION['erreur']?></div><br>
                 <?php
                 unset($_SESSION['erreur']);
             }
             ?>
-            <form method="post" action="index.php?module=connexion&action=inscription">
-                Nom d'utilisateur : <input type="text" name="login" required><br>
-                Mot de passe : <input type="password" name="mdp" required><br>
+            <form method="post" action="index.php?module=connexion&action=inscription" class="animationFormulaireIns">
+                Nom d'utilisateur: <input type="text" name="login" required><br>
+                Mot de passe: <input type="password" name="mdp" required><br>
                 <input type="submit" value="S'inscrire">
             </form>
         </div>
@@ -44,18 +35,18 @@ class VueConnexion extends VueGenerique {
         } else {
             ?>
             <div>
-
+                <link rel="stylesheet" type="text/css" href="modules/mod_connexion/Css.css">
                 <h2 class="connexion">Connexion</h2>
                 <?php if(isset($_SESSION['erreur'])){
                     ?>
-                    <div style="color:red"><?=$_SESSION['erreur']?></div><br>
+                    <div style="color:red; text-align: center;"><?=$_SESSION['erreur']?></div><br>
                     <?php
                     unset($_SESSION['erreur']);
                 }
                 ?>
-                <form method="post" action="index.php?module=connexion&action=connexion">
-                    Nom d'utilisateur : <input type="text" name="login" required><br>
-                    Mot de passe : <input type="password" name="mdp" required><br>
+                <form class="animationFormulaireConn" method="post" action="index.php?module=connexion&action=connexion">
+                    Nom d'utilisateur: <input type="text" name="login" required><br>
+                    Mot de passe: <input type="password" name="mdp" required><br>
                     <input type="submit" value="Se connecter">
                 </form>
             </div>
@@ -64,5 +55,3 @@ class VueConnexion extends VueGenerique {
     }
 }
 ?>
-</body>
-</html>
