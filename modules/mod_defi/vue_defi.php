4e3a46a3e3c8161fcca42aa801099c71ed5edfb9
alaaeddineb <alaaeddine.b15@gmail.com>
2024-01-06 15:58:41 +0100

    ss

--- modules/mod_defi/vue_defi.php
+++ modules/mod_defi/vue_defi.php
@@ -3,65 +3,61 @@ require_once 'vue_generique.php';
 
 class VueDefi extends VueGenerique {
 
-    public function bienvenue() {
-        $cssChemin = "modules/mod_defi/Css-Defi.css";
-    
-        $contenu = isset($_SESSION['user'])
-            ? '
-                <div class="messBien">Bienvenue ' . $_SESSION['user']['pseudo'] . '!</div>
-                <div class="expli">
-                    <div class="box">
-                        <h2>A quoi servent les défis ?</h2>
-                        <p>Vous trouverez ci-dessous une liste de questions de culture général.
-                        <br>Vous avez ensuite un champ qui vous permet de répondre et vous rapporte des jetons visibles sur votre profil.<br>
-                            Ces jetons vous permetteront ensuite d ameliorer vos défenses depuis le site !</p>
-                    </div>
+    public function bienvenue() {    
+        if (isset($_SESSION['user'])) {
+        ?>
+            <div class="messBien">Bienvenue <?php $_SESSION['user']['pseudo'] ?> !</div>
+            <div class="expli">
+                <div class="box">
+                    <h2>A quoi servent les défis ?</h2>
+                    <p>Vous trouverez ci-dessous une liste de questions de culture général.
+                    <br>Vous avez ensuite un champ qui vous permet de répondre et vous rapporte des jetons visibles sur votre profil.<br>
+                        Ces jetons vous permetteront ensuite d ameliorer vos défenses depuis le site !</p>
                 </div>
-                <form action="index.php?module=defi&action=afficheDefi" method="post">
-                    <button type="submit" name="afficheDefi" class="bouttonAff">Afficher Défi</button>
-                </form>'
-            : '<div class="messErr">Pour accéder aux défis, veuillez vous connecter SVP !</div>';
-    
-        echo "
-            <!DOCTYPE html>
-            <html lang='fr'>
+            </div>
+            <form action="index.php?module=defi&action=afficheDefi" method="post">
+                <button type="submit" name="afficheDefi" class="bouttonAff">Afficher Défi</button>
+            </form> <?php
+        }else{
+             ?> <div class="messErr">Pour accéder aux défis, veuillez vous <a href="index.php?module=connexion&action=connexion"> connecter</a> SVP !</div> <?php ;
+        };
+        ?>            
             <head>
                 <meta charset='UTF-8'>
                 <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-                <link rel='stylesheet' type='text/css' href='$cssChemin'>
+                <link rel='stylesheet' type='text/css'  href='modules/mod_defi/Css-Defi.css'>
             </head>
-            <body>
-                $contenu
-            </body>
-            </html>";
+        <?php
     }
     
     function affiche_liste($tab) {
-        echo '<!DOCTYPE html>';
-        echo '<html lang="fr">';
-        echo '<head>';
-        echo '<meta charset="UTF-8">';
-        echo '<meta name="viewport" content="width=device-width, initial-scale=1.0">';
-        echo '<link rel="stylesheet" type="text/css" href="modules/mod_defi/Css-Defi.css">'; 
-        echo '</head>';
-        echo '<body>';
+        ?>
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="stylesheet" type="text/css" href="modules/mod_defi/Css-Defi.css">
+        </head>
+        <?php
 
         foreach($tab as $defi) {
-            echo '<form action="index.php?module=defi&action=traiterReponse" method="post">';
-            echo '<input type="hidden" name="defiId" value="'.$defi['id_defi'].'">'; 
-            echo '<table class="styled-table">';
-            echo '<tr><th>Défi</th><th>Réponse (Tout en minuscule)</th><th>Envoyer</th></tr>';
-            echo '<tr>';
-            echo '<td>'.$defi['defi'].'</td>';
-            echo '<td><input type="text" name="reponse" placeholder="Votre réponse"></td>'; 
-            echo '<td><button type="submit" name="envoyerReponse" class="styled-button">Envoyer Réponse</button></td>';
-            echo '</tr>';
-            echo '</table>';
-            echo '</form>'; 
+            ?>
+            <form action="index.php?module=defi&action=traiterReponse" method="post">
+            <input type="hidden" name="defiId" value="<?php $defi['id_defi'] ?>">
+            <table class="styled-table">
+            <tr><th>Défi</th><th>Réponse (Tout en minuscule)</th><th>Envoyer</th></tr>
+            <tr>
+            <td><?php $defi['defi'] ?> </td>
+            <td><input type="text" name="reponse" placeholder="Votre réponse"></td>
+            <td><button type="submit" name="envoyerReponse" class="styled-button">Envoyer Réponse</button></td>
+            </tr>
+            </table>
+            </form>
+            <?php
         }
-
-        echo '</body>';
-        echo '</html>';
+        ?>        
+        </body>
+        </html>
+        <?php
     }
-    }
-    ?>
+}
+?>
