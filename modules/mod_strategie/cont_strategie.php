26b40162121580b11e1fa0c7b6b3e676440825e0
alaaeddineb <alaaeddine.b15@gmail.com>
2024-01-13 01:50:14 +0100

    Module stratégie

--- modules/mod_strategie/cont_strategie.php
+++ modules/mod_strategie/cont_strategie.php
@@ -14,12 +14,59 @@ class ContStrategie {
         $this->action = isset($_GET['action']) ? $_GET['action'] : "strategie" ;
     }
 
+    function tour() {
+        $this->vue->affiche_listeDefense($this->modele->getListeDefense());
+    }
+
+    function ennemi() {
+        $this->vue->affiche_listeEnnemi($this->modele->getListeAttaqueEtDeplacement());
+    }
+
+    function envoiSuggestion($utilisateur) {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $choix = isset($_POST['choix']) ? $_POST['choix'] : '';
+		    $sug = isset($_POST['sug']) ? $_POST['sug'] : '';
+            $date = $_POST["date"];
+
+            if (!empty($choix) && !empty($sug) && !empty($date)) {
+                
+                if ($this->modele->ajouterSuggestion($utilisateur['id_joueur'], $choix, $sug, $date)) {
+                    $_SESSION["msg"] ="Suggestion envoyé.";
+                    $this->vue->affiche_suggestion();
+                    
+                } else {
+                    $_SESSION["erreur"] = "Erreur lors de l'envoie'.";
+                }
+        
+            } else {
+                $_SESSION["erreur"] = "Veuillez remplir tous les champs du formulaire.";
+            }
+	    }else {
+            $this->vue->affiche_suggestion();
+        }
+        if(isset($_SESSION["erreur"])){
+            $this->vue->affiche_suggestion();
+        }
+    }
 
     function exec(){
 
         switch ($this->action){
             case "strategie":
                 $this->vue->bienvenue();
+                break;
+            case "tour":
+                $this->vue->menu();
+                $this->tour();
+                break;
+            case "ennemi":
+                $this->vue->menu();
+                $this->ennemi();
+                break;    
+            case "suggestion":
+                $this->vue->menu();
+                $utilisateur = $_SESSION['user'];
+                $this->envoiSuggestion($utilisateur);
                 break;
 
             default:
--- modules/mod_strategie/modele_strategie.php
+++ modules/mod_strategie/modele_strategie.php
@@ -11,6 +11,30 @@ class ModeleStrategie extends Connexion{
         $this->connexion::initConnexion();
     }
 
+    function getListeAttaqueEtDeplacement() {
+        $requete = $this->connexion->getBdd()->query("SELECT nom, description FROM strategie WHERE type = 'ennemi';");
+        $tableau = $requete->fetchAll();
+        return $tableau;
+    }
+
+    function getListeDefense() {
+        $requete = $this->connexion->getBdd()->query("SELECT nom, description FROM strategie WHERE type = 'tour';");
+        $tableau = $requete->fetchAll();
+        return $tableau;
+    }
+
+    function getDetail($idEnnemi){
+        $requete = $this->connexion->getBdd()->query("SELECT * FROM ennemi WHERE id_ennemi = '$idEnnemi'");
+        $bio = $requete->fetch(PDO::FETCH_ASSOC);
+        return $bio;
+    }
+
+    function ajouterSuggestion($utilisateur, $choix, $sug, $date) {
+        $requete = $this->connexion->getBdd()->prepare("INSERT INTO suggestion (suggestion, type, date, id_joueur) VALUES (?, ?, ?, ?)");
+        $requete->execute([$sug, $choix, $date, $utilisateur]);
+        return $requete->rowCount() > 0;
+
+    }
 
 }
 
