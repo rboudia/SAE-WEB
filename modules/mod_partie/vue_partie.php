23d176f3d4895e6a5f6c977bb330809a341ea6d5
aboudjemai <aboudjemai@iut.univ-paris8.fr>
2024-01-19 15:38:37 +0100

    css : partie + classement + defi + responsive

--- modules/mod_partie/vue_partie.php
+++ modules/mod_partie/vue_partie.php
@@ -5,141 +5,114 @@ class VuePartie extends VueGenerique{
 
 	public function affiche_barre(){
 		?>
-		<h1>Partie</h1>
-		<h2>Trouvez les statistiques des parties :</h2>
-    	<form action="index.php?module=partie&action=partie" method="post">
-      	<label for="pseudo">Entrez un pseudo :</label><br>
-      	<input type="text" id="pseudo" name="pseudo"><br>
-      	<input type="submit" value="Afficher les statistiques">
-    	</form>
-		<br>
-		<br>
+		<div class="conteneur">
+    <h1 class="titre-principal">Partie</h1>
+    <h2 class="titre-secondaire">Trouvez les statistiques des parties :</h2>
+    <form class="formulaire" action="index.php?module=partie&action=partie" method="post">
+        <label class="label-formulaire" for="pseudo">Entrez un pseudo :</label><br>
+        <input class="champ-texte" type="text" id="pseudo" name="pseudo"><br>
+        <input class="bouton-submit" type="submit" value="Afficher les statistiques">
+    </form>
+    <br>
+    <br>
+</div>
 		<?php 
 	}
 
-	function affiche_liste($tab) {
-        foreach($tab as $element) {
+	public function affiche_liste($tab) {
+        echo '<ul class="liste-parties">';
+        foreach ($tab as $element) {
             ?>
-            <li><?php if(isset($_SESSION['user'])) : ?><a href="index.php?module=partie&action=details&id=<?= $element['id_partie'] ?>"> Partie <?= $element['id_partie'] ?></a> <?php else : ?> Partie <?= $element['id_partie'] ?><?php endif; ?> <?= $element['status'] ?></li>
+            <li class="partie-item">
+                <?php if (isset($_SESSION['user'])) : ?>
+                    <a class="partie-link" href="index.php?module=partie&action=details&id=<?= $element['id_partie'] ?>"> Partie <?= $element['id_partie'] ?></a>
+                <?php else : ?>
+                    Partie <?= $element['id_partie'] ?>
+                <?php endif; ?> <?= $element['status'] ?>
+            </li>
             <?php
-			}
-			if(!isset($_SESSION['user'])){
-				?>
-				<div>Pour accéder aux défis, veuillez vous <a href="index.php?module=connexion&action=connexion"> connecter</a> SVP !</div>
-	            <?php
+        }
+        echo '</ul>';
+        if (!isset($_SESSION['user'])) {
+            ?>
+            <div class="connexion-message">Pour accéder aux défis, veuillez vous <a class="connexion-link" href="index.php?module=connexion&action=connexion">connecter</a> SVP !</div>
+            <?php
         }
     }
 
-	public function classement($elements){
-		$num = 1;
+    public function classement($elements) {
+        $num = 1;
         ?>
-		<section>
-			<h2>Les 3 meilleurs !</h2>
-			<table>
-				<thead>
-					<tr>
-						<th>Classement</th>
-						<th>Pseudo</th>
-					</tr>
-				</thead>
-				<tbody>
-				<?php
-        foreach($elements as $defi) {
-            ?>
-            <tr>
-				<td><?= $num ?></td>
-				<td><a href="index.php?module=classement"><?= $defi['pseudo'] ?></a></td>
-			</tr>
-            </form>
-            <?php
-			$num = $num + 1;
-        }
-        ?> 
+        <section class="classement-section">
+            <h2 class="classement-title">Les 3 meilleurs !</h2>
+            <table class="classement-table">
+                <thead>
+                    <tr>
+                        <th class="classement-header">Classement</th>
+                        <th class="classement-header">Pseudo</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    foreach ($elements as $defi) {
+                        ?>
+                        <tr class="classement-row">
+                            <td class="classement-data"><?= $num ?></td>
+                            <td class="classement-data"><a class="classement-link" href="index.php?module=classement"><?= $defi['pseudo'] ?></a></td>
+                        </tr>
+                        <?php
+                        $num = $num + 1;
+                    }
+                    ?>
+                </tbody>
+            </table>
+        </section>
+        <?php
+    }
 
-				</tbody>
-			</table>
-		</section>		
-		<?php
-	}
-
-	function menu(){
-		if(isset($_SESSION['erreur'])){
+    public function menu() {
+        if (isset($_SESSION['erreur'])) {
             ?>
-            <div style="color:red"><?=$_SESSION['erreur']?></div><br>
+            <div class="erreur-message"><?=$_SESSION['erreur']?></div><br>
             <?php
             unset($_SESSION['erreur']);
         }
-		?>
-		<ul>
-        <?php if(isset($_GET['action']) && $_GET['action'] == 'details') {
-				?>
-			<li><a href="index.php?module=partie">Retour aux parties</a></li>
-			<?php } ?>
+        ?>
+        <ul class="menu-liste">
+            <?php if (isset($_GET['action']) && $_GET['action'] == 'details') : ?>
+                <li class="menu-item"><a class="menu-link" href="index.php?module=partie">Retour aux parties</a></li>
+            <?php endif; ?>
         </ul>
-		<?php
-	}
+        <?php
+    }
 
-
-
-	function affiche_detail($detailEnnemi) {
-			?>
-			<style>
-    table {
-            border-collapse: collapse;
-            width: 80%;
-            max-width: 600px;
-            margin: 20px;
-            background-color: #fff;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-            border-radius: 8px;
-            overflow: hidden;
-        }
-
-        th, td {
-            padding: 15px;
-            text-align: left;
-            border-bottom: 1px solid #ddd;
-        }
-
-        th {
-            background-color: #3498db;
-            color: #fff;
-        }
-
-        td:nth-child(even) {
-            background-color: #ecf0f1;
-        }
-
-        td:nth-child(odd) {
-            background-color: #fff;
-        }
-</style>
-			<center>
-			<table>
-				<tr>
-					<td>Id</td> <td> <?= $detailEnnemi['id_partie'] ?></td>
-				</tr>
-				<tr>
-					<td>Nombre d'ennemis tués</td> <td> <?= $detailEnnemi['nb_ennemis_tues'] ?></td>
-				</tr>
-				<tr>
-					<td>Argent restant</td> <td> <?= $detailEnnemi['argent_restant'] ?></td>
-				</tr>
-				<tr>
-					<td>Vague atteinte</td> <td> <?= $detailEnnemi['vague_atteinte'] ?></td>
-				</tr>
-				<tr>
-					<td>Pv base</td> <td> <?= $detailEnnemi['pv_base'] ?></td>
-				</tr>
-				<tr>
-					<td>Status</td> <td> <?= $detailEnnemi['status'] ?></td>
-				</tr>
-                <tr>
-			</table>
-		</center>
-			<?php
-        
+    public function affiche_detail($detailEnnemi) {
+        ?>
+        <center>
+            <table class="detail-table">
+                <tr class="detail-row">
+                    <td class="detail-label">Id</td> <td class="detail-data"><?= $detailEnnemi['id_partie'] ?></td>
+                </tr>
+                <tr class="detail-row">
+                    <td class="detail-label">Nombre d'ennemis tués</td> <td class="detail-data"><?= $detailEnnemi['nb_ennemis_tues'] ?></td>
+                </tr>
+                <tr class="detail-row">
+                    <td class="detail-label">Argent restant</td> <td class="detail-data"><?= $detailEnnemi['argent_restant'] ?></td>
+                </tr>
+                <tr class="detail-row">
+                    <td class="detail-label">Vague atteinte</td> <td class="detail-data"><?= $detailEnnemi['vague_atteinte'] ?></td>
+                </tr>
+                <tr class="detail-row">
+                    <td class="detail-label">Pv base</td> <td class="detail-data"><?= $detailEnnemi['pv_base'] ?></td>
+                </tr>
+                <tr class="detail-row">
+                    <td class="detail-label">Status</td> <td class="detail-data"><?= $detailEnnemi['status'] ?></td>
+                </tr>
+            </table>
+        </center>
+        <?php
     }
 }
 
 ?>
+<link rel="stylesheet" type="text/css" href="modules\mod_partie\Css-partie.css">
