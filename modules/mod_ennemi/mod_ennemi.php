974be93f076ac83d1c11b2953b76d9f44fab9d9c
rboudia <boudia.rayan77@gmail.com>
2024-01-25 11:37:42 +0100

    correction bug

--- modules/mod_ennemi/mod_ennemi.php
+++ modules/mod_ennemi/mod_ennemi.php
@@ -16,21 +16,3 @@
 
     }
 ?>
-<?php
-    require_once 'cont_ennemi.php';
-
-    class ModEnnemi {
-
-    private $controleur;
-
-    function __construct() {
-        $this->controleur = new ContEnnemi();
-        $this->controleur->exec();
-    }
-
-    public function getAffichage() {
-        return $this->controleur->getAffichage();
-    }
-
-    }
-?>
